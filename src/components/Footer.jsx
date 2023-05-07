71a882674d893afcf75ba98fa2bd30d7c9af1151
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-07 21:06:56 +0400

    footer styling update headers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
                 <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white' >
                   {footerLink.title}
                 </h4>
-                <ul>
+                <ul className='list-none mt-4'>
                   {footerLink.links.map((link, index) => (
                       <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0' }  `}>
                         {link.name}
@@ -29,6 +29,19 @@ const Footer = () => {
               </div>
             ))}
           </div>
+      </div>
+
+      <div className=''>
+        <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+          2023 HooBank. All Rights Reserved.
+        </p>
+
+
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((social, index) => (
+              <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.leng -1 ? 'mr-6' : 'mr-0' }`} />
+          ))}
+        </div>
       </div>
     </section>
   )

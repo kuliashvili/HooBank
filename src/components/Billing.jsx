1ee4f8f61c725622e47af4bbc0819c845cb38f13
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-08 12:33:36 +0400

    add gradients

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -8,8 +8,8 @@ const Billing = () => {
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing"  className='w-[100%] h-[100%] relative z-[5]'/> 
 {/* tu gacheda amis bralia da gamorte */}
-{/* <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
-<div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' /> */}
+<div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+<div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
 
 
       </div>
